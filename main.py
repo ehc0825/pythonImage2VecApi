59f12dddbb8075f011c51199967b5d8d33282331
ehc0825 <ehc0825@naver.com>
2023-01-01 22:07:44 +0900

    image label return plus

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from io import BytesIO
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import requests
+import numpy as np
+import json
 
 
 class UrlItem(BaseModel):
@@ -22,3 +24,31 @@ async def converUrl(item: UrlItem):
     image=Image.open(BytesIO(requestImage.content)).convert('RGB')
     imageVector=img2Vec.get_vec(image)
     return {"vector":imageVector.tolist()}
+
+
+@app.post("/urlImageLabel")
+async def urlImageLabel(item: UrlItem):
+    dicted_item=dict(item)
+    item_path=str(dicted_item['image_url'])
+    model = ResNet50(weights='imagenet')
+    model.summary()
+    res = requests.get(item_path)
+    img = load_img(BytesIO(res.content), target_size=(224, 224))
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    label = decode_predictions(preds, top=3)[0]
+    returnjson = json.dumps(label,cls=NumpyEncoder)
+    return{"label":returnjson}
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
